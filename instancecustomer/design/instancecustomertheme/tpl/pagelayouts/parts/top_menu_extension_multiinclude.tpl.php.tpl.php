58a46e86fbd090ef74e889bf8326f9a59aa37df5
Remdex <remdex@gmail.com>
2014-09-05 19:38:10 +0200

    Few small updates

--- instancecustomer/design/instancecustomertheme/tpl/pagelayouts/parts/top_menu_extension_multiinclude.tpl.php.tpl.php
+++ instancecustomer/design/instancecustomertheme/tpl/pagelayouts/parts/top_menu_extension_multiinclude.tpl.php.tpl.php
@@ -1,2 +1,4 @@
+<?php if ($currentUser->hasAccessTo('lhinstance','billing')) : ?>
 <li><a href="<?php echo erLhcoreClassDesign::baseurl('instance/billing')?>" ><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Billing');?></a></li>
 <li class="divider"></li>
+<?php endif; ?>
