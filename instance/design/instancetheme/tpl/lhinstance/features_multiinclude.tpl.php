3720cb79b996f5d8a1e5a7d594cfd9914a4d3a02
Remdex <remdex@gmail.com>
2015-03-16 17:47:14 +0100

    Hide general features by default

--- instance/design/instancetheme/tpl/lhinstance/features_multiinclude.tpl.php
+++ instance/design/instancetheme/tpl/lhinstance/features_multiinclude.tpl.php
@@ -1,3 +1,6 @@
+<?php 
+
+/* Hide by default
 <div class="col-xs-6">
 	<div class="form-group">
 		<label><input <?php echo $instance->feature_1_supported == 1 ? 'checked="checked"' : ''?> type="checkbox" value="on" name="feature_1_supported"> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('instance/edit','Feature 1')?></label>
@@ -15,3 +18,4 @@
 		<label><input <?php echo $instance->feature_3_supported == 1 ? 'checked="checked"' : ''?> type="checkbox" value="on" name="feature_3_supported"> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('instance/edit','Feature 3')?></label>
 	</div>
 </div>
+*/ ?>
