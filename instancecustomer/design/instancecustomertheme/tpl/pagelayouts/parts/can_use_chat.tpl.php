0de377face32762a7ce93f05896b1142cf9e08f6
Remgijus Kiminas <remdex@gmail.com>
2015-05-10 14:37:27 -0400

    Hide right column if any of chat module is disabled

---
+++ instancecustomer/design/instancecustomertheme/tpl/pagelayouts/parts/can_use_chat.tpl.php
@@ -0,0 +1,1 @@
+<?php $canUseChat = (erLhcoreClassInstance::getInstance()->chat_supported == 1 || erLhcoreClassInstance::getInstance()->chatbox_supported == 1) && erLhcoreClassUser::instance()->hasAccessTo('lhchat','use'); ?> 
