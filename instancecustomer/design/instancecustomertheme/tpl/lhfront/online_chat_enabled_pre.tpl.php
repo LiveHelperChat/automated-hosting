dc80a4229f93ba4d00d82c4a2bb1acb04ab134ed
Remgijus Kiminas <remdex@gmail.com>
2015-05-10 09:41:08 -0400

    Control homepage based on enabled modules

---
+++ instancecustomer/design/instancecustomertheme/tpl/lhfront/online_chat_enabled_pre.tpl.php
@@ -0,0 +1,1 @@
+<?php $online_chat_enabled_pre = erLhcoreClassInstance::getInstance()->chatbox_supported == 1 || erLhcoreClassInstance::getInstance()->chat_supported == 1 || erLhcoreClassInstance::getInstance()->onlinevisitortrck_supported == 1; ?>
