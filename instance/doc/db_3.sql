2a478eacc26d715e0bd57c1f1380433dfe15566f
Remdex <remdex@gmail.com>
2015-05-18 18:03:35 +0200

    Update to 2.32v

--- instance/doc/db_3.sql
+++ instance/doc/db_3.sql
@@ -1218,3 +1218,21 @@ ADD `title` varchar(250) COLLATE 'utf8_general_ci' NOT NULL AFTER `id`,
 ADD `explain` varchar(250) COLLATE 'utf8_general_ci' NOT NULL AFTER `title`,
 ADD `fallback_msg` text COLLATE 'utf8_general_ci' NOT NULL AFTER `msg`,
 COMMENT='';
+
+ALTER TABLE `lh_users`
+ADD `active_chats_counter` int NOT NULL,
+ADD `closed_chats_counter` int NOT NULL AFTER `active_chats_counter`,
+ADD `pending_chats_counter` int NOT NULL AFTER `closed_chats_counter`,
+COMMENT='';
+
+ALTER TABLE `lh_users`
+ADD `departments_ids` varchar(100) NOT NULL,
+COMMENT='';
+
+ALTER TABLE `lh_departament`
+ADD `active_chats_counter` int(11) NOT NULL,
+ADD `pending_chats_counter` int(11) NOT NULL AFTER `active_chats_counter`,
+ADD `closed_chats_counter` int(11) NOT NULL AFTER `pending_chats_counter`,
+COMMENT='';
+
+INSERT INTO `lh_chat_config` (`identifier`, `value`, `type`, `explain`, `hidden`) VALUES ('dashboard_order', 'online_operators,departments_stats|pending_chats,unread_chats|active_chats,closed_chats', '0', 'Home page dashboard widgets order', '0');
