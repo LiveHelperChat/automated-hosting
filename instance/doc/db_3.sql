99c49474b8d50ad8240eb0a604a6034f82b51e6b
Remdex <remdex@gmail.com>
2015-10-31 16:44:12 +0100

    Compatible with 2.39v

--- instance/doc/db_3.sql
+++ instance/doc/db_3.sql
@@ -1294,3 +1294,14 @@ ALTER TABLE `lh_canned_msg` ADD INDEX `position_title` (`position`, `title`);
 
 ALTER TABLE `lh_abstract_widget_theme` ADD `show_need_help` int(11) NOT NULL DEFAULT '1', COMMENT='';
 ALTER TABLE `lh_abstract_widget_theme` ADD `show_need_help_timeout` int(11) NOT NULL DEFAULT '24', COMMENT='';
+
+ALTER TABLE `lh_departament` ADD `visible_if_online` tinyint(1) NOT NULL, COMMENT='';
+ALTER TABLE `lh_abstract_widget_theme` ADD `show_voting` tinyint(1) NOT NULL DEFAULT '1', COMMENT='';
+ALTER TABLE `lh_abstract_widget_theme` ADD `department_title` varchar(250) NOT NULL, COMMENT='';
+ALTER TABLE `lh_abstract_widget_theme` ADD `department_select` varchar(250) NOT NULL, COMMENT='';
+ALTER TABLE `lh_abstract_widget_theme` ADD `buble_visitor_background` varchar(250) NOT NULL, COMMENT='';
+ALTER TABLE `lh_abstract_widget_theme` ADD `buble_visitor_title_color` varchar(250) NOT NULL, COMMENT='';
+ALTER TABLE `lh_abstract_widget_theme` ADD `buble_visitor_text_color` varchar(250) NOT NULL, COMMENT='';
+ALTER TABLE `lh_abstract_widget_theme` ADD `buble_operator_background` varchar(250) NOT NULL, COMMENT='';
+ALTER TABLE `lh_abstract_widget_theme` ADD `buble_operator_title_color` varchar(250) NOT NULL, COMMENT='';
+ALTER TABLE `lh_abstract_widget_theme` ADD `buble_operator_text_color` varchar(250) NOT NULL, COMMENT='';
