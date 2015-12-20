0e78a5e9a72272c85a9d51be89fa9dbb5b0d16f3
Remdex <remdex@gmail.com>
2015-12-20 21:35:09 +0100

    2.40 version

--- instance/doc/db_3.sql
+++ instance/doc/db_3.sql
@@ -1305,3 +1305,8 @@ ALTER TABLE `lh_abstract_widget_theme` ADD `buble_visitor_text_color` varchar(25
 ALTER TABLE `lh_abstract_widget_theme` ADD `buble_operator_background` varchar(250) NOT NULL, COMMENT='';
 ALTER TABLE `lh_abstract_widget_theme` ADD `buble_operator_title_color` varchar(250) NOT NULL, COMMENT='';
 ALTER TABLE `lh_abstract_widget_theme` ADD `buble_operator_text_color` varchar(250) NOT NULL, COMMENT='';
+
+INSERT INTO `lh_chat_config` (`identifier`,`value`,`type`,`explain`,`hidden`) VALUES ('hide_button_dropdown','0','0','Hide close button in dropdown','0');
+INSERT INTO `lh_chat_config` (`identifier`,`value`,`type`,`explain`,`hidden`) VALUES ('on_close_exit_chat','0','0','On chat close exit chat','0');
+ALTER TABLE `lh_departament` ADD `sort_priority` int(11) NOT NULL, COMMENT='';
+ALTER TABLE `lh_departament` ADD INDEX `sort_priority_name` (`sort_priority`,`name`);
