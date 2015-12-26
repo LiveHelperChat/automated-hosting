59a4cfa6b8643d1b9b8a0c524e3a05b80ec8ac33
Remdex <remdex@gmail.com>
2015-12-26 21:02:32 +0100

    Update to 2.41v

--- instance/doc/db_3.sql
+++ instance/doc/db_3.sql
@@ -1310,3 +1310,9 @@ INSERT INTO `lh_chat_config` (`identifier`,`value`,`type`,`explain`,`hidden`) VA
 INSERT INTO `lh_chat_config` (`identifier`,`value`,`type`,`explain`,`hidden`) VALUES ('on_close_exit_chat','0','0','On chat close exit chat','0');
 ALTER TABLE `lh_departament` ADD `sort_priority` int(11) NOT NULL, COMMENT='';
 ALTER TABLE `lh_departament` ADD INDEX `sort_priority_name` (`sort_priority`,`name`);
+
+ALTER TABLE `lh_chat` ADD `unanswered_chat` int(11) NOT NULL DEFAULT '0', COMMENT='';
+ALTER TABLE `lh_chat` ADD INDEX `unanswered_chat` (`unanswered_chat`);
+ALTER TABLE `lh_users` ADD `attr_int_1` int(11) NOT NULL, COMMENT='';
+ALTER TABLE `lh_users` ADD `attr_int_2` int(11) NOT NULL, COMMENT='';
+ALTER TABLE `lh_users` ADD `attr_int_3` int(11) NOT NULL, COMMENT='';
