f659c330fefe0ba479d94203e739b931825d9547
Remdex <remdex@gmail.com>
2016-04-10 11:42:01 +0200

    Default DB

--- instance/doc/db_3.sql
+++ instance/doc/db_3.sql
@@ -1336,3 +1336,16 @@ CREATE TABLE `lh_abstract_rest_api_key` (
   KEY `api_key` (`api_key`),
   KEY `user_id` (`user_id`)
 ) DEFAULT CHARSET=utf8;
+
+CREATE TABLE `lh_admin_theme` (
+       `id` int(11) NOT NULL AUTO_INCREMENT,
+       `name` varchar(100) NOT NULL,
+       `static_content` longtext NOT NULL,
+       `static_js_content` longtext NOT NULL,
+       `static_css_content` longtext NOT NULL,
+       `header_content` text NOT NULL,
+       `header_css` text NOT NULL,
+       PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8;
+
+INSERT INTO `lh_chat_config` (`identifier`, `value`, `type`, `explain`, `hidden`) VALUES ('default_admin_theme_id',	'0',	0,	'Default admin theme',	1);
