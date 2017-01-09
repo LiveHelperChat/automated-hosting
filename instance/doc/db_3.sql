e218f0782b54f3541ced66fbedb1a542f8147ecd
Remdex <remdex@gmail.com>
2017-01-09 19:31:32 +0100

    Update to 2.52v

--- instance/doc/db_3.sql
+++ instance/doc/db_3.sql
@@ -1525,3 +1525,5 @@ CREATE TABLE `lh_departament_group` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+ALTER TABLE `lh_abstract_survey_item` ADD `status` int(11) NOT NULL DEFAULT '0', COMMENT='';
+ALTER TABLE `lh_abstract_survey` ADD `feedback_text` text NOT NULL, COMMENT='';
