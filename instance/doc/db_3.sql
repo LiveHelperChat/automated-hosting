8cbd8ef78fff7e8fc9dca2bb62de7d4b2d01328b
Remdex <remdex@gmail.com>
2018-04-08 20:09:07 +0200

    Update to new version

--- instance/doc/db_3.sql
+++ instance/doc/db_3.sql
@@ -1745,3 +1745,10 @@ CREATE TABLE `lh_group_object` ( `id` bigint(20) NOT NULL AUTO_INCREMENT, `objec
 
 ALTER TABLE `lh_group` ADD `required` tinyint(1) NOT NULL DEFAULT '0', COMMENT='';
 ALTER TABLE `lh_abstract_widget_theme` ADD `modern_look` tinyint(1) NOT NULL DEFAULT '0', COMMENT='';
+
+ALTER TABLE `lh_canned_msg` ADD `additional_data` text NOT NULL, COMMENT='';
+INSERT INTO `lh_chat_config` (`identifier`,`value`,`type`,`explain`,`hidden`) VALUES ('do_no_track_ip','0','0','Do not track visitors IP','0');
+INSERT INTO `lh_chat_config` (`identifier`,`value`,`type`,`explain`,`hidden`) VALUES ('encrypt_msg_after','0','0','After how many days anonymize messages','0');
+INSERT INTO `lh_chat_config` (`identifier`,`value`,`type`,`explain`,`hidden`) VALUES ('encrypt_msg_op','0','0','Anonymize also operators messages','0');
+ALTER TABLE `lh_chat` ADD `anonymized` tinyint(1) NOT NULL DEFAULT '0', COMMENT='';
+ALTER TABLE `lh_chat` ADD INDEX `anonymized` (`anonymized`);
