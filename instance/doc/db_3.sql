e52edd37dceea6467da67b7b7fdb366488e9acf1
Remigijus Kiminas <remdex@gmail.com>
2020-09-03 06:06:08 -0400

    Update to 3.41v

--- instance/doc/db_3.sql
+++ instance/doc/db_3.sql
@@ -2028,3 +2028,7 @@ ALTER TABLE `lh_users_session` ADD `last_error` varchar(255) NOT NULL, COMMENT='
 ALTER TABLE `lh_users_session` ADD INDEX `error` (`error`);
 INSERT INTO `lh_chat_config` (`identifier`, `value`, `type`, `explain`, `hidden`) VALUES
 ('mobile_options',	'a:2:{s:13:\"notifications\";i:0;s:7:\"fcm_key\";s:152:\"AAAAiF8DeNk:APA91bFVHu2ybhBUTtlEtQrUEPpM2fb-5ovgo0FVNm4XxK3cYJtSwRcd-pqcBot_422yDOzHyw2p9ZFplkHrmNXjm8f5f-OIzfalGmpsypeXvnPxhU6Db1B2Z1Acc-TamHUn2F4xBJkP\";}',	0,	'',	1);
+
+ALTER TABLE `lh_chat_blocked_user` ADD `chat_id` int(11) NOT NULL, COMMENT='';
+ALTER TABLE `lh_chat_blocked_user` ADD `dep_id` int(11) NOT NULL, COMMENT='';
+ALTER TABLE `lh_chat_blocked_user` ADD `nick` varchar(50) NOT NULL, COMMENT='';
