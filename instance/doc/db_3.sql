e7ad11bcdfa638e590a23145150ad14faad28fe5
Remigijus Kiminas <remdex@gmail.com>
2020-10-05 09:16:57 -0400

    Update to 3.45v

--- instance/doc/db_3.sql
+++ instance/doc/db_3.sql
@@ -2045,3 +2045,6 @@ CREATE TABLE `lh_generic_bot_command` (
   KEY `dep_id` (`dep_id`),
   KEY `command` (`command`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+ALTER TABLE `lh_abstract_chat_variable` ADD `inv` tinyint(1) NOT NULL, COMMENT='';
+ALTER TABLE `lh_abstract_chat_variable` ADD `change_message` varchar(250) NOT NULL, COMMENT='';
