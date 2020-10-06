6b34ff04f95ca8412f46cfd6cc3794e2a00a4409
Remigijus Kiminas <remdex@gmail.com>
2020-10-06 06:40:12 -0400

    Update to 3.46v

--- instance/doc/db_3.sql
+++ instance/doc/db_3.sql
@@ -2048,3 +2048,12 @@ CREATE TABLE `lh_generic_bot_command` (
 
 ALTER TABLE `lh_abstract_chat_variable` ADD `inv` tinyint(1) NOT NULL, COMMENT='';
 ALTER TABLE `lh_abstract_chat_variable` ADD `change_message` varchar(250) NOT NULL, COMMENT='';
+
+ALTER TABLE `lh_abstract_widget_theme` ADD `widget_show_leave_form` tinyint(1) NOT NULL, COMMENT='';
+ALTER TABLE `lh_abstract_widget_theme` ADD `enable_widget_embed_override` tinyint(1) NOT NULL, COMMENT='';
+ALTER TABLE `lh_abstract_widget_theme` ADD `widget_popheight` int(11) NOT NULL, COMMENT='';
+ALTER TABLE `lh_abstract_widget_theme` ADD `widget_popwidth` int(11) NOT NULL, COMMENT='';
+ALTER TABLE `lh_abstract_widget_theme` ADD `widget_survey` int(11) NOT NULL, COMMENT='';
+ALTER TABLE `lh_abstract_widget_theme` ADD `widget_position` varchar(50) NOT NULL, COMMENT='';
+ALTER TABLE `lh_abstract_widget_theme` ADD `widget_pright` int(11) NOT NULL, COMMENT='';
+ALTER TABLE `lh_abstract_widget_theme` ADD `widget_pbottom` int(11) NOT NULL, COMMENT='';
