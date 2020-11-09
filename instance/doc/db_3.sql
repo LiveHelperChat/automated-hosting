24383151468067cd568ed3c12dc460558417da5b
Remigijus Kiminas <remdex@gmail.com>
2020-11-09 07:48:00 -0500

    Update to 3.50v

--- instance/doc/db_3.sql
+++ instance/doc/db_3.sql
@@ -2071,3 +2071,5 @@ ALTER TABLE `lh_group_chat` ADD INDEX `chat_id` (`chat_id`);
 ALTER TABLE `lh_group_chat_member` ADD `type` tinyint(1) NOT NULL DEFAULT '0', COMMENT='';
 ALTER TABLE `lh_group_chat_member` ADD INDEX `type` (`type`);
 ALTER TABLE `lh_users` ADD INDEX `disabled` (`disabled`);
+
+CREATE TABLE `lh_users_login` (`id` bigint(20) NOT NULL AUTO_INCREMENT,`user_id` bigint(20) NOT NULL,`type` int(11) NOT NULL,`ctime` bigint(20) NOT NULL,`status` int(11) NOT NULL,`ip` varchar(100) NOT NULL,`msg` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,PRIMARY KEY (`id`),KEY `user_id_type` (`user_id`,`type`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
