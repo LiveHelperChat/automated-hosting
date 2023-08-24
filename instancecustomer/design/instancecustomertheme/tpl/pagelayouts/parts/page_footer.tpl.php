fcafc17cd822cbbb95b91c6d6b22d64af3cf1d6c
Remigijus Kiminas <remdex@gmail.com>
2023-08-24 03:28:56 -0400

    Updates

--- instancecustomer/design/instancecustomertheme/tpl/pagelayouts/parts/page_footer.tpl.php
+++
@@ -1,6 +0,0 @@
-<div class="p-1 border-top">
-    <p class="small"><a href="<?php echo erLhcoreClassModelChatConfig::fetch('customer_site_url')->current_value?>"><?php echo htmlspecialchars(erLhcoreClassModelChatConfig::fetch('customer_company_name')->current_value)?></a></p>
-</div>
-
-<?php include_once(erLhcoreClassDesign::designtpl('pagelayouts/parts/page_footer_js.tpl.php'));?>
-<?php include_once(erLhcoreClassDesign::designtpl('pagelayouts/parts/page_footer_js_extension_multiinclude.tpl.php'));?>
--- instancecustomer/design/instancecustomertheme/tpl/pagelayouts/parts/page_footer_user.tpl.php
+++
@@ -1,3 +0,0 @@
-<div class="mt-1 p-1 border-top">
-    <p class="small mb-0"><a href="<?php echo erLhcoreClassModelChatConfig::fetch('customer_site_url')->current_value?>" target="_blank"><?php echo erLhcoreClassModelChatConfig::fetch('customer_company_name')->current_value?></a></p>
-</div>
