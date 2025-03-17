435153cfdde5474eb4a94e7b5efb172721cbac8d
Remigijus Kiminas <remdex@gmail.com>
2025-03-17 10:19:44 -0400

    Update to 4.55v

--- instance/doc/db_3.sql
+++ instance/doc/db_3.sql
@@ -2494,3 +2494,4 @@ ALTER TABLE `lh_users_online_session` ADD `type` tinyint(1) NOT NULL DEFAULT '0'
 ALTER TABLE `lh_departament_custom_work_hours` ADD `repetitiveness` tinyint(1) unsigned NOT NULL DEFAULT '0', COMMENT='';
 ALTER TABLE `lh_departament_custom_work_hours` ADD INDEX `repetitiveness` (`repetitiveness`);
 ALTER TABLE `lh_generic_bot_trigger` ADD `pos` int(11) NOT NULL DEFAULT '0', COMMENT='';
+ALTER TABLE `lh_webhook` ADD `delay` int(11) NOT NULL DEFAULT '0', COMMENT='';
