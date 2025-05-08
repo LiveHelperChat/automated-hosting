e0c4980ad4ce19240723d299d38562ff228d50a5
Remigijus Kiminas <remdex@gmail.com>
2025-05-08 05:52:12 -0400

    Update to 4.60v

--- instance/doc/db_3.sql
+++ instance/doc/db_3.sql
@@ -2397,8 +2397,6 @@ CREATE TABLE `lhc_mailconv_response_template` ( `id` bigint(20) unsigned NOT NUL
 CREATE TABLE `lhc_mailconv_msg_internal` ( `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT, `msg` longtext COLLATE utf8mb4_unicode_ci NOT NULL, `time` bigint(20) unsigned NOT NULL, `chat_id` bigint(20) unsigned NOT NULL DEFAULT 0, `user_id` bigint(20) NOT NULL DEFAULT 0, `name_support` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL, `meta_msg` longtext COLLATE utf8mb4_unicode_ci NOT NULL, PRIMARY KEY (`id`), KEY `chat_id_id` (`chat_id`,`id`), KEY `user_id` (`user_id`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 CREATE TABLE `lhc_mailconv_match_rule` ( `id` int(11) unsigned NOT NULL AUTO_INCREMENT, `dep_id` int(11) unsigned NOT NULL, `conditions` text COLLATE utf8mb4_unicode_ci NOT NULL, `active` tinyint(1) unsigned NOT NULL DEFAULT 1, `mailbox_id` text COLLATE utf8mb4_unicode_ci NOT NULL, `from_name` text COLLATE utf8mb4_unicode_ci NOT NULL, `from_mail` text COLLATE utf8mb4_unicode_ci NOT NULL, `subject_contains` text COLLATE utf8mb4_unicode_ci NOT NULL, `priority` int(11) NOT NULL, `priority_rule` int(11) NOT NULL, `options` text COLLATE utf8mb4_unicode_ci NOT NULL,`name` varchar(50) NOT NULL, PRIMARY KEY (`id`), KEY `active_priority` (`active`,`priority`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-ALTER TABLE `lh_userdep` ADD INDEX `online_op_widget` (`last_activity`,`user_id`,`dep_id`,`always_on`);
-
 ALTER TABLE `lh_abstract_proactive_chat_invitation` ADD `active_from` bigint(20) unsigned NOT NULL DEFAULT '0', COMMENT='';
 ALTER TABLE `lh_abstract_proactive_chat_invitation` ADD `active_to` bigint(20) unsigned NOT NULL DEFAULT '0', COMMENT='';
 ALTER TABLE `lh_abstract_proactive_chat_invitation` ADD `repetitiveness` tinyint(1) unsigned NOT NULL DEFAULT '0', COMMENT='';
@@ -2501,3 +2499,61 @@ ALTER TABLE `lh_audits` ADD `user_id` bigint(20) NOT NULL DEFAULT '0', COMMENT='
 ALTER TABLE `lh_audits` ADD INDEX `user_id` (`user_id`);
 
 CREATE TABLE `lh_notification_op_subscriber` ( `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT, `user_id` bigint(20) unsigned NOT NULL, `device_type` tinyint(1) NOT NULL, `ctime` bigint(20) unsigned NOT NULL, `utime` bigint(20) unsigned NOT NULL, `status` tinyint(1) unsigned NOT NULL, `achat` tinyint(1) unsigned NOT NULL, `pchat` tinyint(1) unsigned NOT NULL, `params` text COLLATE utf8mb4_unicode_ci NOT NULL, `last_error` text COLLATE utf8mb4_unicode_ci NOT NULL, `subscriber_hash` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL, PRIMARY KEY (`id`), KEY `status` (`status`), KEY `user_id` (`user_id`), KEY `subscriber_hash` (`subscriber_hash`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+ALTER TABLE `lh_transfer` ADD INDEX `chat_id_transfer` (`chat_id`,`transfer_scope`), ALGORITHM=INPLACE, LOCK=NONE;
+ALTER TABLE `lh_userdep` ADD INDEX `online_op_widget_2` (`dep_id`, `last_activity`, `user_id`), ALGORITHM=INPLACE, LOCK=NONE;
+ALTER TABLE `lh_userdep` ADD INDEX `online_op_widget_3` (`user_id`, `active_chats`), ALGORITHM=INPLACE, LOCK=NONE;
+
+INSERT INTO `lh_chat_config` (`identifier`,`value`,`type`,`explain`,`hidden`) VALUES ('del_on_close_no_msg','0','0','Delete chat on close if there are no messages from the visitor','0');
+
+
+CREATE TABLE `lh_userdep_disabled` (
+                                       `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                                       `user_id` int(11) NOT NULL,
+                                       `dep_id` int(11) NOT NULL,
+                                       `last_activity` bigint(20) unsigned NOT NULL,
+                                       `hide_online` int(11) NOT NULL,
+                                       `last_accepted` bigint(20) unsigned NOT NULL DEFAULT 0,
+                                       `active_chats` int(11) NOT NULL DEFAULT 0,
+                                       `type` int(11) NOT NULL DEFAULT 0,
+                                       `dep_group_id` int(11) NOT NULL DEFAULT 0,
+                                       `hide_online_ts` bigint(20) unsigned NOT NULL DEFAULT 0,
+                                       `pending_chats` int(11) NOT NULL DEFAULT 0,
+                                       `inactive_chats` int(11) NOT NULL DEFAULT 0,
+                                       `max_chats` int(11) NOT NULL DEFAULT 0,
+                                       `exclude_autoasign` tinyint(1) NOT NULL DEFAULT 0,
+                                       `ro` tinyint(1) NOT NULL DEFAULT 0,
+                                       `always_on` tinyint(1) NOT NULL DEFAULT 0,
+                                       `lastd_activity` bigint(20) unsigned NOT NULL DEFAULT 0,
+                                       `exc_indv_autoasign` tinyint(1) NOT NULL DEFAULT 0,
+                                       `exclude_autoasign_mails` tinyint(1) NOT NULL DEFAULT 0,
+                                       `active_mails` int(11) NOT NULL DEFAULT 0,
+                                       `pending_mails` int(11) NOT NULL DEFAULT 0,
+                                       `max_mails` int(11) NOT NULL DEFAULT 0,
+                                       `last_accepted_mail` bigint(20) unsigned NOT NULL DEFAULT 0,
+                                       `assign_priority` int(11) NOT NULL DEFAULT 0,
+                                       `chat_max_priority` int(11) NOT NULL DEFAULT 0,
+                                       `chat_min_priority` int(11) NOT NULL DEFAULT 0,
+                                       PRIMARY KEY (`id`),
+                                       KEY `dep_id` (`dep_id`),
+                                       KEY `user_id_type` (`user_id`,`type`),
+                                       KEY `last_activity_hide_online_dep_id` (`last_activity`,`hide_online`,`dep_id`),
+                                       KEY `online_op_widget_2` (`dep_id`,`last_activity`,`user_id`),
+                                       KEY `online_op_widget_3` (`user_id`,`active_chats`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `lh_departament_group_user_disabled` (
+                                                      `id` int(11) NOT NULL AUTO_INCREMENT,
+                                                      `dep_group_id` int(11) NOT NULL,
+                                                      `user_id` int(11) NOT NULL,
+                                                      `read_only` tinyint(1) unsigned NOT NULL DEFAULT 0,
+                                                      `exc_indv_autoasign` tinyint(1) unsigned NOT NULL DEFAULT 0,
+                                                      `assign_priority` int(11) NOT NULL DEFAULT 0,
+                                                      `chat_min_priority` int(11) NOT NULL DEFAULT 0,
+                                                      `chat_max_priority` int(11) NOT NULL DEFAULT 0,
+                                                      PRIMARY KEY (`id`),
+                                                      KEY `dep_group_id` (`dep_group_id`),
+                                                      KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `lh_chat_config` (`identifier`, `value`, `type`, `explain`, `hidden`) VALUES ('version_updates',	'328',	0,	'',	1);
